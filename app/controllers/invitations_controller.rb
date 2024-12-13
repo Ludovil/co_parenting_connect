9e8c76f2c171ee2094095a17337c67569c6d46f4
Ludovic Saule <ludovic_saule@hotmail.fr>
2024-12-13 14:27:16 +0100

    invitations unique and cancel

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,12 +1,21 @@
 class InvitationsController < ApplicationController
-  before_action :set_invitation, only: [:accept, :reject]
+  before_action :set_invitation, only: [:accept, :reject, :cancel]
 
   def create
     @invitation = Invitation.new
 
     recipient = User.find_by(email: invitation_params[:recipient_email])
 
+
+
     if recipient
+
+          # Vérifie si le destinataire est déjà un membre de la famille
+    if current_user.family.family_members.exists?(user_id: recipient.id)
+      redirect_to dashboard_path, alert: "Cet utilisateur est déjà membre de votre famille."
+      return
+    end
+
       @invitation.user = current_user  # L'utilisateur qui envoie l'invitation
       @invitation.recipient = recipient  # L'utilisateur destinataire de l'invitation
       @invitation.family = current_user.family  # La famille de l'utilisateur
@@ -34,10 +43,20 @@ class InvitationsController < ApplicationController
     redirect_to dashboard_show_path, notice: 'Invitation rejected!'
   end
 
+
+  def cancel
+    if @invitation.destroy
+      redirect_to dashboard_path, notice: 'Invitation canceled'
+    else
+      redirect_to dashboard_path, alert: 'Something went wrong'
+    end
+  end
+
   private
 
   def set_invitation
     @invitation = Invitation.find(params[:id])
+    redirect_to dashboard_path, alert: "Invitation introuvable." unless @invitation
   end
 
   def invitation_params
