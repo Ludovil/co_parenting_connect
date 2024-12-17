d25b0c73749314015c43e481c89c0b7c59e64c74
Ludovic Saule <ludovic_saule@hotmail.fr>
2024-12-17 18:12:35 +0100

    guard calendar features

--- app/controllers/guards_controller.rb
+++ app/controllers/guards_controller.rb
@@ -14,15 +14,39 @@ class GuardsController < ApplicationController
 
   def new
     @guard = @child.guards.new
+
+    # display only available days //  @taken_days
+    @children = current_user.family ? current_user.family.children : []
+      @children.each do |child|
+      @guards = child.guards
+      @selected_days = []
+      @days_and_guards = []
+       @guards.each do |guard|
+        next if guard.user == current_user
+
+        active_days = guard.attributes.select { |key, value| value == true }.keys
+        @days_and_guards << { days: active_days, user: guard.user.first_name }
+      end
+     end
+    @taken_days = @days_and_guards.flat_map { |entry| entry[:days] }
+
   end
 
   def create
+
+    if current_user.family_member.is_parent
+    # cancel the previous guard pattern for current user
     @child.guards.where(user_id: current_user.id).destroy_all
+
     @guard = @child.guards.new(guard_params)
+
     if @guard.save
       redirect_to child_guards_path(@child), notice: 'Guard was successfully created.'
     else
       render :new, status: :unprocessable_entity
+    end
+    else
+    redirect_to child_guards_path(@child), alert: 'You are not authorized to create a guard for this child.'
     end
   end
 
